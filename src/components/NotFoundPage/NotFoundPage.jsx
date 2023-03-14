dbc7e4f3549140b7ca37b58af58d88c915f31ec5
shehab khallaf <shehabkhallaf122@gmail.com>
2023-03-14 21:51:11 +0200

    not found page

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import notFoundPage from '../../assets/images/NotFoundPage.jpg'
+import { Link } from 'react-router-dom'
+
 
 export default function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <div className='not-Found d-flex justfiy-content-center align-items-center w-100 h-100'>
+      <img className='not-Found-img w-100' src={notFoundPage} alt="" />
+      <div className='not-found-btn btn btn-primary btn-lg rounded-pill'>
+      <Link className="nav-link" to="home">GO TO HOMEPAGE</Link>
+        
+      </div>
+    </div>
   )
 }
