ab93d9012c2f2651daed041ea5cbf1ffeefedac5
shehab khallaf <shehabkhallaf122@gmail.com>
2023-03-17 16:48:36 +0200

    edit navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../../assets/images/logo.jpg'
+// import logo from '../../assets/images/logo.jpg'
 
 
 export default function Navbar(props) {
 
   return (
-    <nav className="navbar fixed-top navbar-expand-lg">
-      <div className="container-fluid">
-        <img src={logo} alt="" />
-        <Link className="navbar-brand fw-bolder" to="home">MDFDD</Link>
+    <nav className="navbar f navbar-expand-lg fixed-top d-flex align-items-center header-transparent">
+      <div className="container-fluid container d-flex justify-content-between align-items-center">
+        {/* <img src={logo} alt="" /> */}
+        <Link className="navbar-brand fw-bolder logo" to="home">MDFDD</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
